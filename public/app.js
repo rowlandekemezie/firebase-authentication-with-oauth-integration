c1506b73b2dc94e38513bc3a4d3dfce3e37abf79
andela-rekemezie <rowland.ekemezie@andela.com>
2016-07-02 12:08:25 +0100

    Remove configuration setting

--- public/app.js
+++ public/app.js
@@ -1,11 +1,10 @@
-  // variable for authentiation
-  var AUTH0_CLIENT_ID = '2HkpGom87ZurLARHEOGqMyNr34XU6dzD';
-  var AUTH0_DOMAIN = 'rownet.auth0.com';
-  var API_KEY = 'AIzaSyBTJyfhWFsbdG0oTC08U45BNqo2WVDPP_I';
-  var FIREBASE_AUTH0_DOMAIN = 'chattbase-14018.firebaseapp.com';
-  var DATABASE_URL = 'https://chattbase-14018.firebaseio.com';
-  var STORAGEBUCKET = 'chattbase-14018.appspot.com';
-
+  // Variable for authentiation
+  var AUTH0_CLIENT_ID = 'fill in your client id';
+  var AUTH0_DOMAIN = 'Your autho domain';
+  var API_KEY = 'Your api key';
+  var FIREBASE_AUTH0_DOMAIN = 'Your firebase domain';
+  var DATABASE_URL = 'Your firebase url';
+  var STORAGEBUCKET = 'Your storage bucket';
 
   // Initialize Firebase
   var config = {
